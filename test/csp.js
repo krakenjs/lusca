91a127267455f82af9b6c7bc1f4f6096699f8eca
Marek Fajkus <marek.faj@gmail.com>
2015-11-07 14:57:35 +0100

    CSP support string and array rules closes #69

--- test/csp.js
+++ test/csp.js
@@ -44,4 +44,37 @@ describe('CSP', function () {
             .expect(200, done);
     });
 
+    it('array block-all-mixed-content + upgrade insecure', function (done) {
+        var app = mock({
+            csp: {
+                policy: ['block-all-mixed-content', 'upgrade-insecure-requests']
+            }
+        });
+
+        app.get('/', function (req, res) {
+            res.status(200).end();
+        });
+
+        request(app)
+            .get('/')
+            .expect('Content-Security-Policy', 'block-all-mixed-content; upgrade-insecure-requests; ')
+            .expect(200, done);
+    });
+
+    it('harcoded block-all-mixed-conntent', function (done) {
+        var app = mock({
+            csp: {
+                policy: 'block-all-mixed-content'
+            }
+        });
+
+        app.get('/', function (req, res) {
+            res.status(200).end();
+        });
+
+        request(app)
+            .get('/')
+            .expect('Content-Security-Policy', 'block-all-mixed-content; ')
+            .expect(200, done);
+    });
 });
