8324cad6c750080220159764963300bea90252c8
Jean-Charles Sisk <jasisk@gmail.com>
2016-04-04 16:00:49 -0400

    tests for better csp config

--- test/csp.js
+++ test/csp.js
@@ -15,6 +15,13 @@ describe('CSP', function () {
     });
 
 
+    it('should throw if misconfigured', function () {
+        assert.throws(function () {
+            lusca.csp(new Date());
+        }, /invalid csp policy/);
+    });
+
+
     it('header (report)', function (done) {
         var config = require('./mocks/config/cspReport'),
             app = mock({ csp: config });
@@ -30,18 +37,61 @@ describe('CSP', function () {
     });
 
 
-    it('header (enforce)', function (done) {
-        var config = require('./mocks/config/cspEnforce'),
+    describe('header (enforce)', function () {
+        it('object config', function (done) {
+            var config = require('./mocks/config/cspEnforce'),
             app = mock({ csp: config });
 
-        app.get('/', function (req, res) {
-            res.status(200).end();
+            app.get('/', function (req, res) {
+                res.status(200).end();
+            });
+
+            request(app)
+                .get('/')
+                .expect('Content-Security-Policy', 'default-src *')
+                .expect(200, done);
         });
 
-        request(app)
-            .get('/')
-            .expect('Content-Security-Policy', 'default-src *; ')
-            .expect(200, done);
-    });
+        it('string config', function (done) {
+            var config = require('./mocks/config/cspString'),
+            app = mock({ csp: config });
 
+            app.get('/', function (req, res) {
+                res.status(200).end();
+            });
+
+            request(app)
+                .get('/')
+                .expect('Content-Security-Policy', 'default-src *')
+                .expect(200, done);
+        });
+
+        it('array config', function (done) {
+            var config = require('./mocks/config/cspArray'),
+            app = mock({ csp: config });
+
+            app.get('/', function (req, res) {
+                res.status(200).end();
+            });
+
+            request(app)
+                .get('/')
+                .expect('Content-Security-Policy', 'default-src *; img-src *')
+                .expect(200, done);
+        });
+
+        it('nested config', function (done) {
+            var config = require('./mocks/config/cspNested'),
+            app = mock({ csp: config });
+
+            app.get('/', function (req, res) {
+                res.status(200).end();
+            });
+
+            request(app)
+                .get('/')
+                .expect('Content-Security-Policy', 'default-src *; img-src *')
+                .expect(200, done);
+        });
+    });
 });
