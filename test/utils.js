9a1ce1ef26f5299de420ec99d8ffce93a14879cd
surya panikkal <surya.com@gmail.com>
2016-05-12 07:45:58 -0400

    fix formatting

--- test/utils.js
+++ test/utils.js
@@ -8,13 +8,13 @@ var utils = require('../lib/utils'),
 describe('utils', function () {
     it('equal strings are true', function () {
         assert.ok(utils.timeSafeCompare('127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935',
-                                      '127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935'),
-                                      'equal strings not equal');
+                                        '127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935'),
+                                        'equal strings not equal');
     });
 
     it('not equal strings are false', function () {
         assert.ok(!utils.timeSafeCompare('alpha',
-                                        'beta'),
-                                        'not equal strings are equal');
+                                         'beta'),
+                                         'not equal strings are equal');
     });
 });
