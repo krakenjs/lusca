26f7b41a864f157dfd80a56c9133273742254629
surya panikkal <surya.com@gmail.com>
2016-05-11 22:51:00 -0400

    fix timing attack vulnerability

---
+++ test/utils.js
@@ -0,0 +1,20 @@
+/*global describe:false, it:false */
+'use strict';
+
+var utils = require('../lib/utils'),
+    assert = require('assert');
+
+
+describe('utils', function () {
+    it('equal strings are true', function () {
+        assert.ok(utils.timeSafeCompare('127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935',
+                                      '127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935'),
+                                      'equal strings not equal');
+    });
+
+    it('not equal strings are false', function () {
+        assert.ok(!utils.timeSafeCompare('alpha',
+                                        'beta'),
+                                        'not equal strings are equal');
+    });
+});
